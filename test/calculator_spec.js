11dfe334c833607f35884fa6eca55594d710f034
Dakota <bazookaharwell@gmail.com>
2017-03-31 08:35:56 -1000

    failing tests

--- test/calculator_spec.js
+++ test/calculator_spec.js
@@ -1,8 +1,22 @@
 // import dependencies e.g. chai and the calculator module
-
+var chai = require('chai');
+var calculator = require('../calculator.js');
 // define two variables: `expect` and `should`.
 
+var expect = chai.expect;
+var should = chai.should;
+
 // define a `describe` block. what are we testing? what should be name this suite? This is the "container" describe block.
+
+describe('calculator module', () => {
+
+  it('should be a function', () => {
+    expect(calculator).to.be.a('function');
+  });
+
+  it('should return an object literal', () => {
+    expect(calculator()).to.be.a('object');
+  });
 
   /**
    * each method should have it's own `describe` block
@@ -31,3 +45,5 @@
    */
 
   // define a `describe` block to test the `validation` features of your methods and all these tests go INSIDE of this describe
+
+});
